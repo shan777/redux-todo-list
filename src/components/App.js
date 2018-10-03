c556a912a4114a41b90de84cb95c544f85bc1a29
Sarah Han <sarah.s.han@gmail.com>
2018-10-03 12:26:29 -0700

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  	render() {
+   	 return (
+		<div className="container">
+			<h1 className="center">To Do App</h1>
+		</div>
+    	);
+  	}
+}
+
+export default App;
